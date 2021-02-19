3260cdbd33052137d147816faa34be96ddb5be60
Mikael Cassamassimo Borges <66427859+yoskatista@users.noreply.github.com>
2021-02-19 12:09:15 -0300

    SaudacaoClientes

---
+++ Saudação.py
@@ -0,0 +1,11 @@
+Nomes = ['Roberto','Rodolfo','Roberto','Ruan','Ronaldo','Admin']
+print('Obrigado por participar ' + Nomes[0])
+print('Obrigado por participar ' + Nomes[1])
+print('Obrigado por participar ' + Nomes[2])
+print('Obrigado por participar ' + Nomes[3])
+print('Obrigado por participar ' + Nomes[4])
+
+if  'Admin' in Nomes:
+    print('Olá admin, gostaria de uma relatório de status?')
+else:
+    print("Obrigado por fazer login " + Nomes[5])
